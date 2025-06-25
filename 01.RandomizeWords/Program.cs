3b01549c19270fd623cd9620a3af60a04da36444
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-25 21:20:51 +0300

    Add project files.

---
+++ 01.RandomizeWords/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * 01.RandomizeWords
+ */
+
+namespace _01.RandomizeWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> words = (Console.ReadLine() ?? "The Devil")
+                .Split()
+                .ToList();
+
+            ShuffleAndPrint(words);
+        }
+
+        static void ShuffleAndPrint<DataType>(List<DataType> collection)
+        {
+            Random randomNumber = new();
+            for (int i = 0; i < collection.Count; i++)
+            {
+                int randomIndex = randomNumber.Next(0, collection.Count);
+                DataType buffer = collection[i];
+                collection[i] = collection[randomIndex];
+                collection[randomIndex] = buffer;
+            }
+
+            collection.ForEach(collectionMember => Console.WriteLine(collectionMember));
+        }
+    }
+}
---
+++ 02.BigFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * 02.BigFactorial
+ */
+
+using System.Numerics;
+
+namespace _02.BigFactorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            ushort number = ushort.Parse(Console.ReadLine() ?? "666");
+            Console.WriteLine(Factorial(number));
+        }
+
+        static BigInteger Factorial(ushort number = 666)
+        {
+            BigInteger baseNumber = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                baseNumber *= i;
+            }
+
+            return baseNumber;
+        }
+    }
+}
